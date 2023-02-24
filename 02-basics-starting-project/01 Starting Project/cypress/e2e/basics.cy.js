ef8e474be7e0bb9ab13127dd26c8c621429cadda
Matthias Schmid-Kietreiber <matthias.schmid-kietreiber@protonmail.com>
2023-02-24 13:18:47 +0100

    finish section 2 basics

---
+++ 02-basics-starting-project/01 Starting Project/cypress/e2e/basics.cy.js
@@ -0,0 +1,16 @@
+/// <reference types="cypress" />
+
+describe("tasks page", () => {
+  it("should render the main image", () => {
+    cy.visit("http://localhost:5173/");
+    cy.get('.main-header').find('img');
+    // cy.get('.main-header img'); // this also works!
+  });
+
+  it('should display the page title', () => {
+    cy.visit('http://localhost:5173/');
+    cy.get('h1').should('have.length', 1);
+    cy.get('h1').contains('My Cypress Course Tasks');
+    // cy.contains('My Cypress Course Tasks');
+  });
+});
